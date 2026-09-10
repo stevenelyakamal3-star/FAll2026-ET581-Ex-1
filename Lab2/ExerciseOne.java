6c52b481199ffe1e6982bd050c5aa9547e7230e8
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-10 22:20:14 +0000

    all changes done

---
+++ Lab2/ExerciseOne.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+
+
+public class ExerciseOne{
+    public static void main(String []args){
+        System.out.println("Enter a String: ");
+        Scanner input = new Scanner(System.in);
+        String str;
+        str = input.nextLine();
+
+
+        int len = str.length();
+        System.out.println(len);
+        input.close();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
