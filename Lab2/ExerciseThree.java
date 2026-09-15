6c188875110fe9c366bb654ea0f00f597d9557ec
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-15 14:12:16 +0000

    all necessary changes made again

--- Lab2/ExerciseThree.java
+++ Lab2/ExerciseThree.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 public class ExerciseThree{
     public static void main(String []args){
         
+        String username = "student";
+
+        
+        
+        
+        
+        
+        System.out.println("Enter username: ");
+        Scanner input = new Scanner(System.in);
+        String inputusername;
+        inputusername = input.nextLine();
+        System.out.println(username.equalsIgnoreCase(inputusername));
+        input.close();
+
+
     }
 
 }
