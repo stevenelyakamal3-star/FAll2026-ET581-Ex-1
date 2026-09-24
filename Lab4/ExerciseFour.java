679c52ee1b789be5ae613a11c1cc34b0efd69274
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-24 01:44:52 +0000

    done

---
+++ Lab4/ExerciseFour.java
@@ -0,0 +1,29 @@
+public class ExerciseFour{
+      public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10, 5));
+        System.out.println(subtract(10, 5));
+        System.out.println(multiply(10, 5));
+        System.out.println(divide(10, 5));
+    }
+
+
+
+
+}
