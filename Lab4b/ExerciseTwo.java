a8edece461d5a5c3a11307285d0ff0ff00431737
Steven Elyakamal <steven.elyakamal3@gmail.com>
2026-09-25 21:30:24 +0000

    changes made

---
+++ Lab4b/ExerciseTwo.java
@@ -0,0 +1,51 @@
+
+import java.util.Random;
+
+
+
+
+
+
+public class ExerciseTwo{
+
+    public static void main(String[] args){
+        
+        
+        
+        
+
+
+
+
+        
+        
+        
+
+        
+        
+        
+        
+        for (int i = 1; i<=3; i++){
+            for (int j = 1; j<=4; j++){
+                
+                int number = (int)(Math.random() *10) + 1;
+                
+                
+                System.out.print(" " + number + " ");
+                
+
+
+
+            }
+
+            System.out.println();
+
+
+        }
+
+
+
+    }
+
+
+}
